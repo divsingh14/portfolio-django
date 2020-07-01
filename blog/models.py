c6dfc41f231ff1be2f24bc197a1ca2c03d2d2636
Divyansh <divsingh14@yahoo.in>
2020-07-01 18:34:56 +0530

    final commit

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
@@ -6,5 +7,18 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+
     def summary(self):
         return self.body[:100]
+
+    def pubDatePretty(self):
+        time = datetime.now()
+        if self.pubDate.day == time.day:
+              return str(time.hour - self.pubDate.hour) + " hours ago"
+        else:
+            if self.month == time.month:
+                return str(time.day - self.pubDate.day) + " days ago"
+
+        return self.pubDate
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Blog
 
 def blogPage(request):
     blogs = Blog.objects
     return render(request,'blog/blogHome.html',{ 'blogs': blogs})
+
+def detailBlog(request,blogId):
+    detailBlog = get_object_or_404(Blog,pk=blogId)
+    return render(request,'blog/blogDetail.html',{ 'blog' : detailBlog})
